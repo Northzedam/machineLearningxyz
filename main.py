79fe76adf97d20cf28b72bd4c949cf165f23bb18
Luciano Aguilera <49349263+Northzedam@users.noreply.github.com>
2024-05-28 18:25:10 -0300

    incluyendo datos de aceleracion en formato aX, iX, aY, iY, aZ, iZ

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ puerto = 1883
 canal = "rmm/generadormtuonsiteenergy/temp"
 
 # Archivo CSV para guardar los datos
-archivo_csv = "datos_conduccion_con_aceleracion.csv"
+archivo_csv = "datosDeSaltoConLosPies.csv"
 
 # Función para manejar el evento de recepción de mensajes MQTT
 def on_message(client, userdata, msg):
@@ -19,7 +19,9 @@ def on_message(client, userdata, msg):
     with open(archivo_csv, "a", newline="") as archivo:
         escritor_csv = csv.writer(archivo)
         datos = mensaje.split(",")  # Ajusta esto según el formato real de tus datos
-        escritor_csv.writerow(datos)
+        if(int(datos[1]) > 16):
+          escritor_csv.writerow(datos)
+          print('accX:'+ str(datos[0]) + ' accY:'+ str(datos[1])+ ' accZ:'+ str(datos[2]) + ' iX:'+ str(datos[3]) + ' iY:'+ str(datos[4]) + ' iZ:'+ str(datos[5]))
 
 # Configuración del cliente MQTT
 cliente = mqtt.Client()
